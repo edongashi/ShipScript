0c2905017a47675218ba9be69dbf9721ccf1e2b4
EdonGashi <edongashi@hotmail.co.uk>
2016-02-22 20:01:58 +0100

    Added simple launcher

---
+++ src/ShipScript.Launcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ClearScript.V8;
+using ShipScript.RShipCore;
+
+namespace ShipScript.Launcher
+{
+    public class Program
+    {
+        private const string Version = "1.0";
+
+        static void Main(string[] args)
+        {
+            var engine = new V8ScriptEngine();
+            engine.AllowReflection = true;
+            var bootstrapper = new RShipCore.Bootstrappers.RShipLoader();
+            bootstrapper.Run(engine, args);
+        }
+    }
+}
